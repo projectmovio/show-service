94683c79867775634e029cb95c8e4e6cdd877085
fulder <misad90@gmail.com>
2020-12-28 22:10:55 +0100

    Remove copy pasted anime refs from show_by_id lambda

--- src/lambdas/api/show_by_id/__init__.py
+++ src/lambdas/api/show_by_id/__init__.py
@@ -1,10 +1,10 @@
 import json
 
-import anime_db
+import show_db
 import decimal_encoder
 import logger
 
-log = logger.get_logger("anime_by_id")
+log = logger.get_logger("show_by_id")
 
 
 class HttpError(object):
@@ -14,9 +14,9 @@ class HttpError(object):
 def handle(event, context):
     log.debug(f"Received event: {event}")
 
-    anime_id = event["pathParameters"].get("id")
+    show_id = event["pathParameters"].get("id")
 
-    res = anime_db.get_anime_by_id(anime_id)
+    res = show_db.get_show_by_id(show_id)
     return {
         "statusCode": 200,
         "body": json.dumps(res, cls=decimal_encoder.DecimalEncoder)
