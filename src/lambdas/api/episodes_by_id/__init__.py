7e1dfb2f286551745efed2fc57268e8c58d3d2f3
fulder <misad90@gmail.com>
2021-01-27 23:30:28 +0100

    Add lambda for getting one show episode

---
+++ src/lambdas/api/episodes_by_id/__init__.py
@@ -0,0 +1,29 @@
+import json
+
+import episodes_db
+import shows_db
+import decimal_encoder
+import logger
+
+log = logger.get_logger("episodes_by_id")
+
+
+class HttpError(object):
+    pass
+
+
+def handle(event, context):
+    log.debug(f"Received event: {event}")
+
+    show_id = event["pathParameters"].get("id")
+    episode_id = event["pathParameters"].get("episode_id")
+
+    try:
+        res = episodes_db.get_episode_by_id(show_id, episode_id)
+    except shows_db.NotFoundError:
+        return {"statusCode": 404}
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(res, cls=decimal_encoder.DecimalEncoder)
+    }
