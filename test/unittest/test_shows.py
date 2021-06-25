938c4e0c9e4fa7c6b809b519d3f82ea368024b33
fulder <misad90@gmail.com>
2021-06-25 12:35:51 +0200

    Refactor shows tests to method classes

--- test/unittest/test_shows.py
+++ test/unittest/test_shows.py
@@ -1,105 +1,8 @@
+import copy
 import json
 import pytest
 
 from api.shows import handle, UnsupportedMethod
-
-
-def test_post_shows(mocked_shows_db):
-    mocked_shows_db.table.query.side_effect = mocked_shows_db.NotFoundError
-    event = {
-        "requestContext": {
-            "http": {
-                "method": "POST"
-            }
-        },
-        "body": '{"api_id": "123", "api_name": "tvmaze"}'
-    }
-
-    res = handle(event, None)
-
-    exp = {
-        "body": json.dumps({"id": "cf1ffb71-48c3-53c0-9966-900cc5e5553e"}),
-        "statusCode": 200
-    }
-    assert res == exp
-
-
-def test_post_shows_already_exist(mocked_shows_db):
-    mocked_shows_db.table.query.return_value = {
-        "Items": [
-            {
-                "tvmaze_id": "123"
-            }
-        ]
-    }
-    event = {
-        "requestContext": {
-            "http": {
-                "method": "POST"
-            }
-        },
-        "body": '{"api_id": "123", "api_name": "tvmaze"}'
-    }
-
-    res = handle(event, None)
-
-    exp = {
-        "body": json.dumps({"id": "cf1ffb71-48c3-53c0-9966-900cc5e5553e"}),
-        "statusCode": 200
-    }
-    assert res == exp
-
-
-def test_post_shows_no_body(mocked_shows_db):
-    mocked_shows_db.table.query.return_value = {
-        "Items": [
-            {
-                "tvmaze_id": "123"
-            }
-        ]
-    }
-    event = {
-        "requestContext": {
-            "http": {
-                "method": "POST"
-            }
-        },
-    }
-
-    res = handle(event, None)
-
-    exp = {
-        "statusCode": 400,
-        "body": "Invalid post body"
-    }
-    assert res == exp
-
-
-def test_post_shows_invalid_body(mocked_shows_db):
-    mocked_shows_db.table.query.return_value = {
-        "Items": [
-            {
-                "mal_id": 123
-            }
-        ]
-    }
-    event = {
-        "requestContext": {
-            "http": {
-                "method": "POST"
-            }
-        },
-        "body": '{"aa": "bb"}'
-    }
-
-    res = handle(event, None)
-
-    exp = {
-        'body': '{"message": "Invalid post schema", '
-                '"error": "Additional properties are not allowed (\'aa\' was unexpected)"}',
-        'statusCode': 400
-    }
-    assert res == exp
 
 
 def test_unsupported_method():
@@ -118,15 +21,85 @@ def test_unsupported_method():
         handle(event, None)
 
 
-def test_get_by_api_id(mocked_shows_db):
-    exp_res = {
-        "id": "123"
+class TestPost:
+    event = {
+        "requestContext": {
+            "http": {
+                "method": "POST"
+            }
+        },
+        "body": '{"api_id": "123", "api_name": "tvmaze"}'
     }
-    mocked_shows_db.table.query.return_value = {
-        "Items": [
-            exp_res
-        ]
-    }
+
+    def test_success(self, mocked_shows_db):
+        mocked_shows_db.table.query.side_effect = mocked_shows_db.NotFoundError
+
+        res = handle(self.event, None)
+
+        exp = {
+            "body": json.dumps({"id": "cf1ffb71-48c3-53c0-9966-900cc5e5553e"}),
+            "statusCode": 200
+        }
+        assert res == exp
+
+    def test_already_exist(self, mocked_shows_db):
+        mocked_shows_db.table.query.return_value = {
+            "Items": [
+                {
+                    "tvmaze_id": "123"
+                }
+            ]
+        }
+
+        res = handle(self.event, None)
+
+        exp = {
+            "body": json.dumps({"id": "cf1ffb71-48c3-53c0-9966-900cc5e5553e"}),
+            "statusCode": 200
+        }
+        assert res == exp
+
+    def test_no_body(self, mocked_shows_db):
+        mocked_shows_db.table.query.return_value = {
+            "Items": [
+                {
+                    "tvmaze_id": "123"
+                }
+            ]
+        }
+        event = copy.deepcopy(self.event)
+        del event["body"]
+
+        res = handle(event, None)
+
+        exp = {
+            "statusCode": 400,
+            "body": "Invalid post body"
+        }
+        assert res == exp
+
+    def test_invalid_body(self, mocked_shows_db):
+        mocked_shows_db.table.query.return_value = {
+            "Items": [
+                {
+                    "mal_id": 123
+                }
+            ]
+        }
+        event = copy.deepcopy(self.event)
+        event["body"] = '{"aa": "bb"}'
+
+        res = handle(event, None)
+
+        exp = {
+            'body': '{"message": "Invalid post schema", '
+                    '"error": "Additional properties are not allowed (\'aa\' was unexpected)"}',
+            'statusCode': 400
+        }
+        assert res == exp
+
+
+class TestGet:
     event = {
         "requestContext": {
             "http": {
@@ -139,73 +112,56 @@ def test_get_by_api_id(mocked_shows_db):
         }
     }
 
-    res = handle(event, None)
-
-    exp = {
-        "statusCode": 200,
-        "body": json.dumps(exp_res)
-    }
-    assert res == exp
-
-
-def test_get_by_api_id_not_found(mocked_shows_db):
-    mocked_shows_db.table.query.side_effect = mocked_shows_db.NotFoundError
-    event = {
-        "requestContext": {
-            "http": {
-                "method": "GET"
-            }
-        },
-        "queryStringParameters": {
-            "api_id": "123",
-            "api_name": "tvmaze",
+    def test_success(self, mocked_shows_db):
+        exp_res = {
+            "id": "123"
         }
-    }
-
-    res = handle(event, None)
-
-    exp = {
-        "statusCode": 404,
-    }
-    assert res == exp
-
-
-def test_get_no_query_params():
-    event = {
-        "requestContext": {
-            "http": {
-                "method": "GET"
-            }
-        },
-        "queryStringParameters": {
+        mocked_shows_db.table.query.return_value = {
+            "Items": [
+                exp_res
+            ]
         }
-    }
 
-    res = handle(event, None)
+        res = handle(self.event, None)
 
-    exp = {
-        "statusCode": 400,
-        "body": json.dumps({"error": "Please specify query parameters"})
-    }
-    assert res == exp
+        exp = {
+            "statusCode": 200,
+            "body": json.dumps(exp_res)
+        }
+        assert res == exp
 
+    def test_not_found(self, mocked_shows_db):
+        mocked_shows_db.table.query.side_effect = mocked_shows_db.NotFoundError
 
-def test_get_invalid_query_params():
-    event = {
-        "requestContext": {
-            "http": {
-                "method": "GET"
-            }
-        },
-        "queryStringParameters": {
+        res = handle(self.event, None)
+
+        exp = {
+            "statusCode": 404,
+        }
+        assert res == exp
+
+    def test_empty_query_params(self):
+        event = copy.deepcopy(self.event)
+        event["queryStringParameters"] = {}
+
+        res = handle(event, None)
+
+        exp = {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Please specify query parameters"})
+        }
+        assert res == exp
+
+    def test_invalid_query_params(self):
+        event = copy.deepcopy(self.event)
+        event["queryStringParameters"] = {
             "abc": "123"
         }
-    }
 
-    res = handle(event, None)
+        res = handle(event, None)
 
-    exp = {
-        "statusCode": 400,
-        "body": json.dumps({"error": "Missing api_id query parameter"})
-    }
-    assert res == exp
+        exp = {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Missing api_id query parameter"})
+        }
+        assert res == exp
