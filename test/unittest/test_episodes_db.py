84d5aee723a0f05e1295476d5c25c9ff285e1768
Michal Sadowski <misad90@gmail.com>
2021-07-02 08:52:54 +0200

    Revert "Use get_item in get_episode_by_id function"

--- test/unittest/test_episodes_db.py
+++ test/unittest/test_episodes_db.py
@@ -28,3 +28,12 @@ def test_get_episode_by_id_not_found(mocked_episodes_db):
     with pytest.raises(mocked_episodes_db.NotFoundError):
         mocked_episodes_db.get_episode_by_id("456", "123")
 
+
+def test_get_episode_by_id_invalid_count(mocked_episodes_db):
+    mocked_episodes_db.table.query.return_value = {
+        "Items": [{"episode_id": "123"}],
+        "Count": 2
+    }
+
+    with pytest.raises(mocked_episodes_db.InvalidAmountOfEpisodes):
+        mocked_episodes_db.get_episode_by_id("456", "123")
