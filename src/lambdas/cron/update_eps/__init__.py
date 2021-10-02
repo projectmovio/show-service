52150ff910ed8cd6663da372633ff8578d635fac
fulder <misad90@gmail.com>
2021-10-02 13:38:37 +0200

    Start adding cron lambda for update_eps SNS messages

---
+++ src/lambdas/cron/update_eps/__init__.py
@@ -0,0 +1,18 @@
+import shows_db
+from tvmaze import TvMazeApi
+
+
+def handle(event, context):
+    tvmaze_api = TvMazeApi()
+    tvmaze_updates = tvmaze_api.get_day_updates()
+
+    for tvmaze_id in tvmaze_updates:
+        try:
+            shows_db.get_show_by_api_id("tvmaze", tvmaze_id)
+        except shows_db.NotFoundError:
+            # Show not present in db, exclude it from updates
+            continue
+
+        # Post to SNS topic
+
+
